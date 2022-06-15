6e89ebfea983bd1f40e7ebb7d535166134f5541b
Elizabeth Rios-Mujica <elizabeth.rmujica@gmail.com>
2022-06-14 22:06:22 -0400

    commit tree 90a85968b24934decd40bb0b581d6e1a10fbb83f

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+
+
+  def full_name
+    # Write a method in the Actor class, #full_name, that returns the first and last name of an actor.
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    # Write a method in the Actor class, #list_roles, that lists all of the characters that actor has alongside the show that the character is in.
+    self.characters.map do |character|
+        "#{character.name} - #{character.show.name}"
+    end
+  end
 end
